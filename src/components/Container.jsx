45a94f21fbd2a98ae92012d492fa327adda6e7f8
Alexandre Leitao <alexandre.leitao94350@gmail.com>
2024-10-01 11:25:36 +0200

    feature - new button

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 const Container = () => {
     return (
@@ -24,6 +25,9 @@ const Container = () => {
                             <p>Humidité: <span>60%</span></p>
                             <p>Vent: <span>15 km/h</span></p>
                         </div>
+                        <div className="footer-button-container">
+                            <Link href="/paris" className="footer-button"> Voir la météo de Paris</Link>
+                        </div>
                     </div>
                 </div>
 
@@ -46,6 +50,9 @@ const Container = () => {
                         <div className="footer">
                             <p>Humidité: <span>40%</span></p>
                             <p>Vent: <span>10 km/h</span></p>
+                        </div>
+                        <div className="footer-button-container">
+                            <Link href="/New-York" className="footer-button"> Voir la météo de New York</Link>
                         </div>
                     </div>
                 </div>
@@ -70,6 +77,9 @@ const Container = () => {
                             <p>Humidité: <span>50%</span></p>
                             <p>Vent: <span>5 km/h</span></p>
                         </div>
+                        <div className="footer-button-container">
+                            <Link href="/Tokyo" className="footer-button"> Voir la météo de Tokyo</Link>
+                        </div>
                     </div>
                 </div>
 
@@ -92,6 +102,9 @@ const Container = () => {
                         <div className="footer">
                             <p>Humidité: <span>50%</span></p>
                             <p>Vent: <span>5 km/h</span></p>
+                        </div>
+                        <div className="footer-button-container">
+                            <Link href="/Tokyo" className="footer-button"> Voir la météo de Tokyo</Link>
                         </div>
                     </div>
                 </div>
@@ -116,6 +129,9 @@ const Container = () => {
                             <p>Humidité: <span>50%</span></p>
                             <p>Vent: <span>5 km/h</span></p>
                         </div>
+                        <div className="footer-button-container">
+                            <Link href="/Tokyo" className="footer-button"> Voir la météo de Tokyo</Link>
+                        </div>
                     </div>
                 </div>
 
@@ -139,69 +155,52 @@ const Container = () => {
                             <p>Humidité: <span>50%</span></p>
                             <p>Vent: <span>5 km/h</span></p>
                         </div>
+                        <div className="footer-button-container">
+                            <Link href="/Tokyo" className="footer-button"> Voir la météo de Tokyo</Link>
+                        </div>
                     </div>
                 </div>
 
                 {/* Carte météo 7 */}
-                <div className="card">
-                    <div className="card2">
-                        <div className="header">
-                            <h2>Tokyo, JP</h2>
-                            <p>Mardi 17 Septembre 2024</p>
-                        </div>
-                        <div className="body">
-                            <div className="temp">25°C</div>
-                            <div className="icon">
-                                <Image src={"/svg/Ensoleille.svg"} alt="Weather Icon" width={50} height={50} />
-                            </div>
-                            <div className="description">
-                                <p>Ensoleillé</p>
-                            </div>
-                        </div>
-                        <div className="footer">
-                            <p>Humidité: <span>50%</span></p>
-                            <p>Vent: <span>5 km/h</span></p>
-                        </div>
+<div className="card-longues">
+    <div className="card3">
+        <div className="header">
+            <h2>Météo du jour - Paris</h2>
+            <p>Mardi 17 Septembre 2024</p>
+        </div>
+        <div className="body">
+            <div className="temp">25°C</div>
+            <div className="icon">
+                <Image src={"/svg/Ensoleille.svg"} alt="Weather Icon" width={50} height={50} />
+            </div>
+            <div className="description">
+                <p>Ensoleillé</p>
+            </div>
+        </div>
+        <div className="footer">
+            <div className="hourly-forecast">
+                {Array.from({ length: 24 }, (_, i) => (
+                    <div className="hour" key={i}>
+                        <p>{String(i).padStart(2, '0')}:00</p>
+                        <p>{25 - i}°C</p>
                     </div>
-                </div>
-                {/* Carte météo 7 */}
-                <div className="card-longues">
-                    <div className="card3">
-                        <div className="header">
-                            <h2>Météo du jour - Paris</h2>
-                            <p>Mardi 17 Septembre 2024</p>
-                        </div>
-                        <div className="body">
-                            <div className="temp">25°C</div>
-                            <div className="icon">
-                                <Image src={"/svg/Ensoleille.svg"} alt="Weather Icon" width={50} height={50} />
-                            </div>
-                            <div className="description">
-                                <p>Ensoleillé</p>
-                            </div>
-                        </div>
-                        <div className="footer">
-                        <div className="hourly-forecast">
-                            {Array.from({ length: 24 }, (_, i) => (
-                                <div className="hour" key={i}>
-                                    <p>{String(i).padStart(2, '0')}:00</p>
-                                    <p>{25 - i}°C</p>
-                                </div>
-                            ))}
-                        </div>
-                        </div>
-                        
-                    </div>
-                </div>
+                ))}
+            </div>
+        </div>
+    </div>
+</div>
+                
             </div>
 
             <div className="map iframe">
                 <iframe
-                    src="https://earth.nullschool.net/#current/wind/surface/level/orthographic=2.06,24.44,241"frameBorder="0"></iframe>
+                    src="https://earth.nullschool.net/#current/wind/surface/level/orthographic=2.06,24.44,241"
+                    frameBorder="0"></iframe>
             </div>
             <div className="mobile-map iframe">
                 <iframe
-                    src="https://earth.nullschool.net/#current/wind/surface/level/orthographic=2.06,24.44,177"frameBorder="0"></iframe>
+                    src="https://earth.nullschool.net/#current/wind/surface/level/orthographic=2.06,24.44,177"
+                    frameBorder="0"></iframe>
             </div>
         </div>
     );
